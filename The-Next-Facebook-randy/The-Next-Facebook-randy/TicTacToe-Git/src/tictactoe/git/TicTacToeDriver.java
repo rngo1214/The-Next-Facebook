40f2a093db0ced1374ff7a7fe4c10727cc5f1d9f
2006218 <2006218@AL-105-18.student.acalanes.k12.ca.us>
2017-03-27 11:17:33 -0700

    tictactoe driver

---
+++ The-Next-Facebook-randy/The-Next-Facebook-randy/TicTacToe-Git/src/tictactoe/git/TicTacToeDriver.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe.git;
+import java.util.Scanner;
+/**2
+ * 
+ *
+ * @author 2006218
+ */
+public class TicTacToeDriver {
+    public static void main(String args[]) {
+        TicTacToe TicTacToeGame = new TicTacToe();
+        TicTacToeGame.newGame();
+    }
+}
