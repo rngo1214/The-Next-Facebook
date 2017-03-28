41daf71553f552869d6c6e652bc2ab59da46b885
2006218 <2006218@AL-105-18.student.acalanes.k12.ca.us>
2017-03-28 11:45:16 -0700

    final tictactoe

--- The-Next-Facebook-randy/The-Next-Facebook-randy/TicTacToe-Git/src/tictactoe/git/TicTacToe.java
+++ The-Next-Facebook-randy/The-Next-Facebook-randy/TicTacToe-Git/src/tictactoe/git/TicTacToe.java
@@ -9,40 +9,56 @@ package tictactoe.git;
  * @author 2006218
  */
 public class TicTacToe extends Game{
-    //instance variables
+    //instance variables, one for each potential player
     Player player1;
     Player player2;
+    //utilizes board class
     Board gameBoard = new Board(3,3);
+    //determines the state of the game, if it is completed and there is a winner or not
     boolean win;
     
     public TicTacToe(){
         super();
     }
+    //this is the main loop, lets the players take their turns
     public void gameLoop() {
         while (win == false) {
-            playerTurn(player1);         
-            playerTurn(player2);
+            
+            playerTurn(player1); 
+            if (win == false){
+                playerTurn(player2);
+            }
         }
     }
-    
+    //all of the code for the player to take their turn
     public void playerTurn(Player player) {
         System.out.println();
+        //takes in the player input for where they want to place their marker
         System.out.println(player.getName() + ", Choose Row (1-3):");
-        int row = scan.nextInt() - 1;
+        int row = scan.nextInt() - 1; //subtracts one so the user input matches the array index
         System.out.println(player.getName() + ", Choose Column (1-3):");
         int column = scan.nextInt() - 1;
         if (validInput(row) && validInput(column)) {
+            //checks to see if the user input a valid location before placing the marker
              markBoard(row, column, player);
         }
         else {
+            //repeats the input recursively until the player inputs a valid location
             System.out.println("Invalid input. Please retry:");
             playerTurn(player);
         }
+        //draws the board with the new marker in place
         gameBoard.drawBoard();
         if (isWinner(player, row, column)) {
             System.out.println();
             win = true;
             endGame(player.getName());
+        }
+        else {
+            if (checkTie()) {
+                //checks to see if all spaces on the board are full, then declares nobody as the winner
+                endGame("Nobody");
+            }
         }
         
     }
@@ -50,7 +66,7 @@ public class TicTacToe extends Game{
         int x = 0;
         for (int i = 0; i < gameBoard.getBoard().length; i ++){
             for (int a = 0; a < gameBoard.getBoard()[i].length; a++) {
-                if (!gameBoard.getBoard()[i][a].equals("")) {
+                if (!gameBoard.getBoard()[i][a].equals(" ")) {
                     x++;
                 }
             }
@@ -93,7 +109,7 @@ public class TicTacToe extends Game{
             DiagonalWinOne = false;
             DiagonalWinTwo = false;
         }
-
+        
         boolean hasWon = (HorizontalWin || VerticalWin || DiagonalWinOne || DiagonalWinTwo);
 
         return hasWon;
@@ -129,4 +145,29 @@ public class TicTacToe extends Game{
             playerTurn(player);
         }
     }
+    
+    public void newGame(){
+        win = false;
+        System.out.println("Players? (1/2)");
+        int players = scan.nextInt();
+        while (!validInput(players)){
+            System.out.println("Please enter a valid input");
+            players = scan.nextInt();
+        }
+        if (players == 1){
+            System.out.println("Enter your name, Player 1");
+            String playerName = scan.nextLine();
+            startGame(playerName);
+        }
+        else if (players == 2){
+            System.out.println("Enter your name, Player 1");
+            String p1Name = "";
+            while (p1Name.equals("")){
+                p1Name = scan.nextLine(); //weird workaround
+            }
+            System.out.println("Enter your name, Player 2");
+            String p2Name = scan.nextLine();
+            startGame(p1Name, p2Name);
+        }
+    }     
 }
